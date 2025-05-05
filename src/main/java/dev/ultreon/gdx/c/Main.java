896d34e2256681eb7104e7d3613267fb7d09952e
Qubix <xypercode@ultreon.dev>
2025-05-05 20:35:04 +0200

    Different errors???

--- src/main/java/dev/ultreon/gdx/c/Main.java
+++ src/main/java/dev/ultreon/gdx/c/Main.java
@@ -2,17 +2,23 @@ package dev.ultreon.gdx.c;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.graphics.GL32;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import dev.ultreon.gdx.c.glfw.GLFW;
 import dev.ultreon.gdx.c.opengl.OpenGL;
 
 public class Main implements ApplicationListener {
+    private SpriteBatch batch;
+    private Texture texture;
+
     public static void main(String[] args) {
         NativeApplication application = new NativeApplication(new Main());
     }
 
     @Override
     public void create() {
-
+        batch = new SpriteBatch();
+        texture = new Texture("badlogic.png");
     }
 
     @Override
@@ -22,7 +28,9 @@ public class Main implements ApplicationListener {
 
     @Override
     public void render() {
-
+        batch.begin();
+        batch.draw(texture, 0, 0);
+        batch.end();
     }
 
     @Override
@@ -37,6 +45,7 @@ public class Main implements ApplicationListener {
 
     @Override
     public void dispose() {
-
+        batch.dispose();
+        texture.dispose();
     }
 }
